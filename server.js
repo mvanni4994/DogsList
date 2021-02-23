a1a78b8bc13294fb7abe8574802f1704e6ae2d3a
Laura Aupert <aupert.laura@gmail.com>
2021-02-22 21:34:51 -0500

    Added Handlebars npm

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const htmlRouter = require('./routes/html-routes.js');
 const ownerRouter = require('./routes/owner-api-routes.js');
@@ -14,15 +15,15 @@ const Strategy = require('passport-local').Strategy;
 // (`username` and `password`) submitted by the user.  The function must verify
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
+// passport.use(new Strategy(
+//   function(username, password, cb) {
+//     db.users.findByUsername(username, function(err, user) {
+//       if (err) { return cb(err); }
+//       if (!user) { return cb(null, false); }
+//       if (user.password != password) { return cb(null, false); }
+//       return cb(null, user);
+//     });
+//   }));
 
 // Configure Passport authenticated session persistence.
 //
@@ -31,22 +32,25 @@ passport.use(new Strategy(
 // typical implementation of this is as simple as supplying the user ID when
 // serializing, and querying the user record by ID from the database when
 // deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
+// passport.serializeUser(function(user, cb) {
+//   cb(null, user.id);
+// });
 
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
+// passport.deserializeUser(function(id, cb) {
+//   db.users.findById(id, function (err, user) {
+//     if (err) { return cb(err); }
+//     cb(null, user);
+//   });
+// });
 // End of passport starter code
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Handlebars engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 // Requiring our models for syncing
 const db = require('./models');
 
