27f168af393e55f287264c45f66a15e51cd11bb9
njazgunovich <nicholasjaz@gmail.com>
2021-02-25 14:52:04 -0500

    added s3

---
+++ config/config.js
@@ -0,0 +1,23 @@
+console.log(process.env)
+module.exports = {
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": "rehoming",
+        "host": "127.0.0.1",
+        "port": 3306,
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": "Tstai.410",
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "port": 3306,
+        "dialect": "mysql"
+    },
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql"
+    }
+}
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
+require("dotenv").config()
 const express = require('express');
 const exphbs = require('express-handlebars');
 const htmlRouter = require('./routes/html-routes.js');
 const ownerRouter = require('./routes/owner-api-routes.js');
 const apiRouter = require('./routes/post-api-routes.js');
-
-//S3 starter code
-const aws = require('aws-sdk');
-
-
-// Sets up the Express App
+const s3Router = require('./routes/s3-api-routes.js')
+    // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -27,16 +24,10 @@ app.use(express.static('public'));
 htmlRouter(app);
 ownerRouter(app);
 apiRouter(app);
+s3Router(app);
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
 });
-
-//More S3 code 
-const S3_BUCKET = process.env.S3_BUCKET;
-// WE MUST CHANGE TO THE RIGHT REGION
-aws.config.region = 'us-east-2';
-//
-
