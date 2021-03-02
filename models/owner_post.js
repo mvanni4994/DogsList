1bb55918e69aa4a7789811835c06d930ccd7c0c8
Laura Aupert <aupert.laura@gmail.com>
2021-03-02 01:17:34 -0500

    Style: font and cards across pages. SQL: added email validation. Nav and footer.

--- models/owner_post.js
+++ models/owner_post.js
@@ -60,7 +60,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
+        isEmail: true,
       },
     },
     owners_address: {
